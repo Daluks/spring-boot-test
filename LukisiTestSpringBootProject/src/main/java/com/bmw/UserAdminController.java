c563290b019f0c09408dda769da9f51e25cfd22a
QXU1114 <QXU1114@LW09736441.africa.bmw.corp>
2018-02-20 14:18:22 +0200

    New project created

---
+++ LukisiTestSpringBootProject/src/main/java/com/bmw/UserAdminController.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bmw;
+
+import com.bmw.model.UserDTO;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author QXU1114
+ */
+@RestController
+public class UserAdminController
+{
+
+    @RequestMapping("/users")
+    public List<UserDTO> getAllUsers()
+    {
+        List<UserDTO> users = new ArrayList();
+        UserDTO user = new UserDTO();
+        user.setName("Test");
+        user.setSurname("test");
+        user.setUserId("QXU1114");
+        users.add(user);
+        
+        UserDTO user2 = new UserDTO();
+        user2.setName("name");
+        user2.setSurname("surname");
+        user2.setUserId("QXU1114");
+        users.add(user2);
+        return users;
+    }
+    
+    
+}
